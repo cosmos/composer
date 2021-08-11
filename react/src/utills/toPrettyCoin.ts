1c7ebf94f04b4a112fb10db24308ea1ff9905791
Dmytro-Kyparenko <69693013+Dmytro-Kyparenko@users.noreply.github.com>
2021-08-11 15:11:14 +0300

    displaying all info for CPSProposal

--- react/src/utills/toPrettyCoin.ts
+++ react/src/utills/toPrettyCoin.ts
@@ -1,6 +1,9 @@
 import { CoinPretty, Dec } from "@keplr-wallet/unit";
 import { chainInfo } from "../config";
 
-export const toPrettyCoin = (amount: string | Dec): CoinPretty => {
+export const toPrettyCoin = (
+    amount: string | Dec,
+    denom: string = chainInfo.currencies[0].coinMinimalDenom
+): CoinPretty => {
     return new CoinPretty(chainInfo.currencies[0], new Dec(amount.toString()));
 };

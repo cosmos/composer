943fb429254f99e1a1a509097d96f13ce58377be
Vladimir Mitin <vmitin.dev@gmail.com>
2021-09-13 14:23:20 +0300

    vue: removed from git

--- vue/src/store/generated/cosmos/admin-module/cosmos.adminmodule.adminmodule/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { TextProposal } from "./module/types/adminmodule/adminmodule";
-import { Proposal } from "./module/types/adminmodule/adminmodule";
-export { TextProposal, Proposal };
-declare const _default;
-export default _default;
